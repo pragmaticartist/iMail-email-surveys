d7b75bdd6512eca50168922387143b6a4d0f58d8
Talima <business.teamTALIMA@gmail.com>
2019-03-24 13:55:31 -0700

    refactor: change greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ subscribe: 'Pewdiepie' });
+  res.send({ subscribe: 'to pewdiepie' });
 });
 
 const PORT = process.env.PORT || 5000;
