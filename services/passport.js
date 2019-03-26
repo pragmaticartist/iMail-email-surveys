8e445b594aeefa697f9378e8f716aa395f979c50
Talima <business.teamTALIMA@gmail.com>
2019-03-26 15:40:20 -0700

    feat: add proxy support

--- services/passport.js
+++ services/passport.js
@@ -21,6 +21,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       // find user with googleId of profile.id
