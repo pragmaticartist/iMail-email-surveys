8c5ad0b79990bb09c368429fd4a7d97eb389402b
Talima <business.teamTALIMA@gmail.com>
2019-03-26 15:27:40 -0700

    feat: add dev and prod keys

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,6 +9,15 @@ module.exports = app => {
     })
   );
 
-  // authentication attempt with 'code' from Google
+  // authentication attempt with oauth user identification 'code' from Google
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
